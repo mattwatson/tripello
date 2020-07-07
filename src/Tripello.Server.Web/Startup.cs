5c8d1565e193ca3a04d6d6187aa28ffe036ed0be
Matt Watson <mattwatson1@gmail.com>
2020-07-07 01:25:41 +0100

    tidy up

--- src/Tripello.Server.Web/Startup.cs
+++ src/Tripello.Server.Web/Startup.cs
@@ -57,6 +57,7 @@ namespace Tripello.Server.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseStatusCodePages();
             
             app.UseForwardedHeaders(
